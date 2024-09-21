0627857e17005e60aaa3e3e65c525e0757f27041
osnatshabtay <osnatshabtay98@gmail.com>
2024-09-21 23:06:42 +0300

    added util tests

--- app/api/contacts/utils.py
+++ app/api/contacts/utils.py
@@ -1,5 +1,15 @@
+import re
+
 def is_valid_phone_number(phone_number):
-    return phone_number is not None
+    if phone_number is None:
+        return False
+    
+    pattern = re.compile(r'^\+?\d{10,15}$')
+    return bool(pattern.match(phone_number))
 
 def is_valid_name(first_name, last_name):
-    return True
+    if first_name is None:
+        return False
+    
+    pattern = re.compile(r"^[A-Za-z\s\-']{1,100}$")
+    return bool(pattern.match(first_name)) and bool(pattern.match(last_name) if last_name is not None else True)
