dcf5469a1c952620fe4a1f26a03aa6cfff54bce2
osnatshabtay <osnatshabtay98@gmail.com>
2024-09-23 23:42:42 +0300

    workings

--- tests/app_tests/endpoints_tests/test_get_contacts.py
+++ tests/app_tests/endpoints_tests/test_get_contacts.py
@@ -10,16 +10,18 @@ from app.api.contacts.models import Contact
 from app.common.db import get_db
 from app.common.utils import get_env_variable
 from tests.utils import random_phone_number, random_string
+from datetime import datetime
 
 class TestGetContact:
 
     @classmethod
     def setup_class(cls):
-        cls.number_of_contacts = 15
+        cls.number_of_not_deleted_contacts = 15
+        cls.number_of_deleted_contacts = 1
         async def inner():
             phone_numbers = set()
             async for db_conn in get_db():
-                for i in range(cls.number_of_contacts):
+                for i in range(cls.number_of_not_deleted_contacts):
                     # Generate unique phone number
                     phone_number = random_phone_number()
                     while phone_number in phone_numbers:
@@ -33,6 +35,20 @@ class TestGetContact:
                         address=random_string()
                     )
                     db_conn.add(new_contact)
+                
+                # add one contact for deleted_ts test
+                phone_number = random_phone_number()
+                while phone_number in phone_numbers:
+                    phone_number = random_phone_number()
+                phone_numbers.add(f"{phone_number}")
+                new_contact = Contact(
+                        first_name=random_string(), 
+                        last_name=random_string(),
+                        phone_number=phone_number, 
+                        address=random_string(),
+                        deleted_ts=datetime.now()
+                    )
+                db_conn.add(new_contact)
 
                 await db_conn.commit()
         asyncio.get_event_loop().run_until_complete(inner())
@@ -48,8 +64,8 @@ class TestGetContact:
                     await db_conn.commit()
                     
                     deleted_count = result.rowcount
-                    if deleted_count != cls.number_of_contacts:
-                        raise Exception(f"deleted count({deleted_count}) != contacts_number({cls.number_of_contacts})")
+                    if deleted_count != cls.number_of_not_deleted_contacts + cls.number_of_deleted_contacts:
+                        raise Exception(f"deleted count({deleted_count}) != contacts_number({cls.number_of_not_deleted_contacts + + cls.number_of_deleted_contacts})")
             except Exception as e:
                 logging.error(f"Error while deleting contacts - {e}")
                 raise e
@@ -80,15 +96,14 @@ class TestGetContact:
                         assert len(fetched_contacts) > 0, f"0 contacts returned"
                         break
 
-                    # assert last page contains self.number_of_contacts % limit contacts
-                    assert len(response_dict) == limit or len(response_dict) == self.number_of_contacts % limit, f"fetched response does not contain the expected number of results. limit={limit}, offset={offset}, response_len={len(response_dict)}, last_page={self.number_of_contacts % limit}"
+                    # assert last page contains self.number_of_not_deleted_contacts % limit contacts
+                    assert len(response_dict) == limit or len(response_dict) == self.number_of_not_deleted_contacts % limit, f"fetched response does not contain the expected number of results. limit={limit}, offset={offset}, response_len={len(response_dict)}, last_page={self.number_of_not_deleted_contacts % limit}"
 
                     # update offset
                     offset += limit
 
 
-                assert len(fetched_contacts) == self.number_of_contacts, f"not all contact returned. fetched_contacts({len(fetched_contacts)})={fetched_contacts}\n != {self.number_of_contacts}"
-
+                assert len(fetched_contacts) == self.number_of_not_deleted_contacts, f"not all contact returned. fetched_contacts({len(fetched_contacts)})={fetched_contacts}\n != {self.number_of_not_deleted_contacts}"
 
         asyncio.get_event_loop().run_until_complete(inner())
 
